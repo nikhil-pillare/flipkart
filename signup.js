f1e3763d5b90499595ddee1fe9765fb848f30c15
unknown <nikhilpillare98@gmail.com>
2022-11-30 13:57:34 +0530

    signup done

---
+++ signup.js
@@ -0,0 +1,10 @@
+let username= "masai";
+let password= "masai@123";
+
+let loginUsername= "masai";
+let loginPassword= "masi@123";
+
+
+
+if(username==loginUsername && password==loginUsername){
+console.log("you are welcome")};
